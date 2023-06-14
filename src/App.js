c309b49724ab403f82dbafc7d923dd3cd020e02b
dhananjay bhagwat <dhananjaybhagwat777@gmail.com>
2023-06-14 14:58:47 +0530

    interpolation with objects

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 //Interpolation :- means data binding in jsx
 
+// interpolation using object literal
 
 function App() {
-  let str = `Hello world`;
-  let pi = 3.131;
-  let  num = 21;
-  let active = true ;
-   let isLogin = true ;
-
+  let admin = {id:21 ,name:"dhananjay"};
+  
   return (
     <>
-      <h1>HELLO WORLD</h1>
-      <h1> pi = {pi} !!</h1>
-      <h1>{str}</h1>
-
-      {/**login logout logic */}
-      {isLogin && <h1>Welcome to Cdac.!!</h1> }
-
-
-
-      {/** uses of boolean */}
-      <h1>{active ?"software developer":"web developer"}</h1>
-
-      {/**most usefulll */}
-      {active && <h1>Its true</h1> }
-      {active ? <h1>Its true</h1>: <h1>is False</h1> }
+      
+      <h1>
+        ID : {admin.id} <br />
+        Name : {admin.name}
+      </h1>
     </>
   );                
 }
