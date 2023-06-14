9a583e3c3055b4dc0748ba2b6abcd69760f6a543
dhananjay bhagwat <dhananjaybhagwat777@gmail.com>
2023-06-14 15:19:23 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-
-
-// interpolation using object literal
-
+// HTML + JS => JSX
+// Putting the Brain in HTML.
+// Styling :: Color
+// CSS is also JavaScript!!!
 function App() {
-  let id = "p1";
-  let str = "never give up"
-  
+  // object literals
+  let style = { background: "green", color: "white" };
+
   return (
     <>
-      <h1 id="p1">hello jerry</h1>
-      <h1 id={"p1"}>hello tom</h1>
-      <h1 id={"p1"}>hello nick</h1>
-      <h1 id={`p1`}>hello chris</h1>
-      <h1 id={'p1'}> {str}</h1>
-      <h1 id={'p1'}> {id}</h1>
+      <h1 style={style}>Hello World</h1>
+
+      {/** First Curly Brace represents JS Expression */}
+      {/** Second Culry Brace, its object literal */}
+      <h1 style={{}}>Hello</h1>
+      <h1 style={{ background: "blue", color: "white" }}>Hello Again</h1>
+
+      {/** Observations */}
+      {/** Style Attributes become CamelCase in Nature */}
+      {/** Values we are wriing in double quotes */}
+      {/** Multiple props are seperate by comma */}
+
+      <h1 style={{ backgroundColor: "red", color: "white", padding: "8px" }}>
+        hello World
+      </h1>
     </>
-  );                
+  );
 }
+
 export default App;
