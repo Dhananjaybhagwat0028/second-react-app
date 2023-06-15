a529815d3805efd7c91195c2096ba263f2eb6536
dhananjay bhagwat <dhananjaybhagwat777@gmail.com>
2023-06-15 14:42:25 +0530

    bootstarp basic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import "./App.css";
+
 //import "bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
     <>
-      <h1>hello globe</h1>
+      <h1 className="bg-primmary text-bg-success p-3 d-flex justify-content-center">welcome to cdac</h1>
+
+      <div className="d-flex">
+      <div className="  alert alert-secondary border border-danger-subtle m-3">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam sit enim unde a recusandae doloremque provident nulla totam eligendi doloribus! Fugiat, vitae totam praesentium reiciendis illum cumque facilis esse natus, a eius nulla quia consequuntur. Libero ipsam magni quod. Animi porro soluta modi cumque corrupti praesentium est rem numquam unde!z
+      </div>
+      <div className="  alert alert-secondary border border-danger-subtle m-3">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam sit enim unde a recusandae doloremque provident nulla totam eligendi doloribus! Fugiat, vitae totam praesentium reiciendis illum cumque facilis esse natus, a eius nulla quia consequuntur. Libero ipsam magni quod. Animi porro soluta modi cumque corrupti praesentium est rem numquam unde!z
+      </div>
+      <div className="  alert alert-secondary border border-danger-subtle m-3">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam sit enim unde a recusandae doloremque provident nulla totam eligendi doloribus! Fugiat, vitae totam praesentium reiciendis illum cumque facilis esse natus, a eius nulla quia consequuntur. Libero ipsam magni quod. Animi porro soluta modi cumque corrupti praesentium est rem numquam unde!z
+      </div>
+
+
+      </div>
+
+
+
+
     </>
   );
 }
