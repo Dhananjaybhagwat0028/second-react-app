2a4d159e993d3a494f13f3379bedb2bb03231a5b
dhananjay bhagwat <dhananjaybhagwat777@gmail.com>
2023-06-15 14:21:35 +0530

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-
-/**class is a keyword in  js so we use className */
-import "./App.css"
-function App(){
-  return(
+import "./App.css";
+//import "bootstrap/dist/css/bootstrap.css";
+function App() {
+  return (
     <>
-    <h1 className="beauty"> Wonderlust.**</h1>
-    <h1 style={{color:"red"}}>kedarnath to paris</h1>
+      <h1>hello globe</h1>
     </>
   );
-  
 }
- export default App;
+export default App;
